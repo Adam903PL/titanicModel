75a22df500683b6f299b73860e9214097ee8d3cf
Adam Pukaluk <pukaluk.adam505@gmail.com>
2025-04-28 14:04:23 +0200

    init2

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
+
 import pandas as pd
 import numpy as np
-
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 
+# Wczytanie danych .csv
 train_data = pd.read_csv('data.csv')
 
+# Usunięcie kolumn, które nie są potrzebne
 train_data = train_data.drop(['Name', 'Ticket', 'Cabin'], axis=1)
 
+# Wydzielenie zmiennej celu (czy pasażer przeżył) i cech
 y = train_data['Survived']
 X = train_data.drop(['Survived'], axis=1)
 
+
 X = pd.get_dummies(X, drop_first=True)
 
+# Uzupełnienie brakujących wartości w kolumnie 'Age' średnią
 X.fillna({'Age': X['Age'].mean()}, inplace=True)
 
+# Dodatkowe uzupełnienie brakujących wartości w 'Embarked' (jeśli istnieje)
 if 'Embarked' in X.columns:
     X.fillna({'Age': X['Age'].mean()}, inplace=True)
 
+# Podział danych na zbiór treningowy i testowy (80% / 20%)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Standaryzacja danych - skalowanie do średniej 0 i odchylenia standardowego 1
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-
+# Trenowanie modelu Random Forest
 model_rf = RandomForestClassifier(n_estimators=100, random_state=42)
 model_rf.fit(X_train_scaled, y_train)
 y_pred_rf = model_rf.predict(X_test_scaled)
@@ -36,10 +44,11 @@ y_pred_rf = model_rf.predict(X_test_scaled)
 accuracy_rf = accuracy_score(y_test, y_pred_rf)
 print(f'Random Forest Accuracy: {accuracy_rf}')
 
-
+# Trenowanie modelu Regresji
 model_lr = LogisticRegression(max_iter=1000, random_state=42)
 model_lr.fit(X_train_scaled, y_train)
 y_pred_lr = model_lr.predict(X_test_scaled)
 
+# Ocena dokładności modelu Regresji Logistycznej
 accuracy_lr = accuracy_score(y_test, y_pred_lr)
 print(f'Logistic Regression Accuracy: {accuracy_lr}')
